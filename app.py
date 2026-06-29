7e04995b3b1b3c58f2ac6bf6f28feda45bd6978b
Yogesh <yogeshmadhukumar86@gmail.com>
2026-06-29 11:51:17 +0530

    added title label and task input field

--- app.py
+++ app.py
@@ -3,4 +3,13 @@ import tkinter as tk
 root = tk.TK()
 root.title("To Do App")
 root.geometry("450x650")
+root.configure(bg="#000000")
+
+tk.label(root, text="Tasks", font=("Arial", 22, "bold"),
+         bg="#000000", fg="#D4AF37").pack(pady=25)
+
+task_entry = tk.Entry(root, font=("Arial", 18), bg="#EEEEEE",
+                        fg="#000000", borderwidth=0, justify="center")
+task_Entry.pack(pady=10, padx=30, fill=tk.X)
+
 root.mainloop()
