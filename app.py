df4c8686b2fc1ddcb22a6b258e6a0cdf5496180f
Yogesh <yogeshmadhukumar86@gmail.com>
2026-06-29 11:57:56 +0530

    added task listbox to display tasks

--- app.py
+++ app.py
@@ -12,4 +12,11 @@ task_entry = tk.Entry(root, font=("Arial", 18), bg="#EEEEEE",
                         fg="#000000", borderwidth=0, justify="center")
 task_Entry.pack(pady=10, padx=30, fill=tk.X)
 
+
+# listbox to show all tasks
+tasks_list = tk.Listbox(root, font=("Arial", 14), bg="#111111",
+                        fg="#FFFFFF", selecrbackground= "#D4AF37",
+                        selectforeground="#000000", borderwidth=0)
+tasks_list.pack(fill=tk.BOTH, expamd=True, padx=30, pady=20)
+
 root.mainloop()
