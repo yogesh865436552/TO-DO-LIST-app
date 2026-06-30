91a9f8ee0a98941dfd8c7c63dbf87ca1160371cb
Yogesh <yogeshmadhukumar86@gmail.com>
2026-06-30 12:08:23 +0530

    added add and delete buttons

--- app.py
+++ app.py
@@ -10,10 +10,18 @@ tk.label(root, text="Tasks", font=("Arial", 22, "bold"),
 
 task_entry = tk.Entry(root, font=("Arial", 18), bg="#EEEEEE",
                         fg="#000000", borderwidth=0, justify="center")
-task_Entry.pack(pady=10, padx=30, fill=tk.X)
+task_entry.pack(pady=10, padx=30, fill=tk.X)
 
+# add and delete buttons side by side
+btn_frame = tk.Frame(root, bg="#000000")
+btn_frame.pack(pady=15)
 
-# listbox to show all tasks
+tk.Button(btn_frame, text="ADD TASK", font=("Arial", 12, "bold"),
+          bg="#EEEEEE", fg="#000000", width=12).pack(side=tk.LEFT, padx=10)
+
+tk.Button(btn_frame, text="DELETE", font=("Arial", 12,"bold"),
+          bg="#EEEEEE", fg="#FFFFFF", width=12).pack(side=tk.LEFT, padx=10)
+
 tasks_list = tk.Listbox(root, font=("Arial", 14), bg="#111111",
                         fg="#FFFFFF", selecrbackground= "#D4AF37",
                         selectforeground="#000000", borderwidth=0)
